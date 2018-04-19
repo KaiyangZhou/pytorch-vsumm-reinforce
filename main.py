44157c2204d4c75c3fb8097abb46fe371408b955
KaiyangZhou <k.zhou@qmul.ac.uk>
2018-04-19 17:37:55 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,141 @@
+from __future__ import print_function
+import os
+import os.path as osp
+import argparse
+import sys
+import h5py
+import time
+import datetime
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from torch.autograd import Variable
+from torch.optim import lr_scheduler
+from torch.distributions import Bernoulli
+
+from utils import Logger, read_json
+from models import *
+
+parser = argparse.ArgumentParser("Pytorch code for unsupervised video summarization with REINFORCE")
+# Dataset options
+parser.add_argument('-d', '--dataset', type=str, required=True, help="path to h5 dataset (required)")
+parser.add_argument('-s', '--split', type=str, required=True, help="path to split file (required)")
+parser.add_argument('--split-id', type=int, default=0, help="split index (default: 0)")
+# Model options
+parser.add_argument('--input-dim', type=int, default=1024, help="input dimension (default: 1024)")
+parser.add_argument('--hidden-dim', type=int, default=256, help="hidden unit dimension of DSN (default: 256)")
+parser.add_argument('--num-layers', type=int, default=1, help="number of RNN layers (default: 1)")
+parser.add_argument('--rnn-cell', type=str, default='lstm', help="RNN cell type (default: lstm)")
+# Optimization options
+parser.add_argument('--lr', type=float, default=1e-05, help="learning rate (default: 1e-05)")
+parser.add_argument('--weight-decay', type=float, default=1e-05, help="weight decay rate (default: 1e-05)")
+parser.add_argument('--max-epoch', type=int, default=60, help="maximum epoch for training (default: 60)")
+parser.add_argument('--stepsize', type=int, default=30, help="how many steps to decay learning rate (default: 30)")
+parser.add_argument('--gamma', type=float, default=0.1, help="learning rate decay (default: 0.1)")
+parser.add_argument('--num-episode', type=int, default=5, help="number of episodes (default: 5)")
+parser.add_argument('--beta', type=float, default=0.01, help="weight for summary length penalty term (default: 0.01)")
+# Misc
+parser.add_argument('--seed', type=int, default=1, help="random seed (default: 1)")
+parser.add_argument('--gpu', type=str, default='0', help="which gpu devices to use")
+parser.add_argument('--use-cpu', action='store_true', help="use cpu device")
+parser.add_argument('--evaluate', action='store_true', help="whether to do evaluation only")
+parser.add_argument('--save-dir', type=str, default='log', help="path to save output (default: 'log/')")
+parser.add_argument('--resume', type=str, default='', help="path to resume file")
+
+args = parser.parse_args()
+
+torch.manual_seed(args.seed)
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+use_gpu = torch.cuda.is_available()
+if args.use_cpu: use_gpu = False
+
+def main():
+    if not args.evaluate:
+        sys.stdout = Logger(osp.join(args.save_dir, 'log_train.txt'))
+    else:
+        sys.stdout = Logger(osp.join(args.save_dir, 'log_test.txt'))
+    print("==========\nArgs:{}\n==========".format(args))
+
+    if use_gpu:
+        print("Currently using GPU {}".format(args.gpu))
+        cudnn.benchmark = True
+        torch.cuda.manual_seed_all(args.seed)
+    else:
+        print("Currently using CPU")
+
+    print("Initializing dataset {}".format(args.dataset))
+    dataset = h5py.File(args.dataset, 'r')
+    num_videos = len(dataset.keys())
+    splits = read_json(args.split)
+    assert args.split_id < len(splits), "split_id (got {}) exceeds {}".format(args.split_id, len(splits))
+    split = splits[args.split_id]
+    train_keys = split['train_keys']
+    test_keys = split['test_keys']
+    print("# total videos {}. # train videos {}. # test videos {}".format(num_videos, len(train_keys), len(test_keys)))
+
+    print("Initializing model")
+    model = DSN(in_dim=args.input_dim, hid_dim=args.hidden_dim, num_layers=args.num_layers, cell=args.rnn_cell)
+    print("Model size: {:.5f}M".format(sum(p.numel() for p in model.parameters())/1000000.0))
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    if args.stepsize > 0:
+        scheduler = lr_scheduler.StepLR(optimizer, step_size=args.stepsize, gamma=args.gamma)
+
+    if args.resume:
+        print("Loading checkpoint from '{}'".format(args.resume))
+        checkpoint = torch.load(args.resume)
+        model.load_state_dict(checkpoint['state_dict'])
+        start_epoch = checkpoint['epoch']
+    else:
+        start_epoch = 0
+
+    if use_gpu:
+        model = nn.DataParallel(model).cuda()
+
+    if args.evaluate:
+        print("Evaluate only")
+        return
+
+    print("==> Start training")
+    start_time = time.time()
+
+    for epoch in range(start_epoch, args.max_epoch):
+        idxs = np.arange(len(train_keys))
+        np.random.shuffle(idxs) # shuffle data indices
+
+        for idx in idxs:
+            key = train_keys[idx]
+            seq = dataset[key]['features'][...]
+            seq = torch.from_numpy(seq).unsqueeze(0) # (1, seq_len, dim)
+            if use_gpu: seq = seq.cuda()
+            seq = Variable(seq)
+            probs = model(seq) # (1, seq_len, 1)
+
+            cost = 0.
+            cost += args.beta * (probs.mean() - 0.5)**2 # minimize summary length penalty term [Eq.11]
+            m = Bernoulli(probs)
+            for _ in range(args.num_episode):
+                actions = m.sample()
+                log_probs = m.log_prob(actions)
+                #reward = compute_reward() # TODO
+                reward = 1. # delete later
+                expected_reward = log_probs.mean() * reward
+                cost -= expected_reward # minimize negative expected reward
+
+            optimizer.zero_grad()
+            cost.backward()
+            optimizer.step()
+            print("cost = {}".format(cost.data[0]))
+
+        print("Done epoch {}".format(epoch+1))
+
+    elapsed = round(time.time() - start_time)
+    elapsed = str(datetime.timedelta(seconds=elapsed))
+    print("Finished. Total elapsed time (h:m:s): {}".format(elapsed))
+
+    dataset.close()
+
+if __name__ == '__main__':
+    main()
---
+++ rewards.py
@@ -0,0 +1,10 @@
+import torch
+
+def compute_div_reward():
+    pass
+
+def compute_rep_reward():
+    pass
+
+def compute_reward():
+    pass
