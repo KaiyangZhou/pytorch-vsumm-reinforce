61aae5ac8fac6c1d5dccbabedd98948a696fb475
KaiyangZhou <k.zhou@qmul.ac.uk>
2018-04-20 12:08:50 +0100

    add instruction

--- parse_json.py
+++ parse_json.py
@@ -7,6 +7,14 @@ matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 from utils import read_json
 
+"""
+Parse json file (.json) to extract rewards for specific videos.
+
+How to use:
+# image will be saved in path: blah_blah_blah
+$ python parse_json.py -p blah_blah_blah/rewards.json -i 0
+"""
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--path', type=str, required=True, help="path to rewards.json; output saved to the same dir")
 parser.add_argument('-i', '--idx', type=int, default=0, help="choose which video to visualize, index starts from 0 (default: 0)")
